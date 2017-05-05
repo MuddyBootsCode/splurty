a68813ee901fe9627c1522f20ece01caa23f4a3a
Michael Porter <firsttogo98@gmail.com>
2017-05-05 11:17:37 +0000

    Fixed quote cycle

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,6 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require tether
 //= require bootstrap-sprockets
+//= require jquery_ujs
 //= require_tree .
